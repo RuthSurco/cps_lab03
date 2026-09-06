7f1ab1804a7bc5d48b8a1c968fa4eab2dec32340
Ruth <ruth.surco@tecsup.edu.pe>
2026-09-05 22:47:25 -0500

    Cambio final del Responsable

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -4,4 +4,9 @@ public class StudentController {
     public void ejecutar() {
         System.out.println("Controlador: Procesando vista para Sprint-2");
     }
+
+    // Cambio final realizado por el Responsable en Sprint-2
+    public void unificar() {
+        System.out.println("Controlador: Unificación de cambios finalizada por el Responsable");
+    }
 }
